16d6ce734929f15bcdfa18aa1f7f90ccab64a67f
vvpvvp <alias8516@sina.com.cn>
2018-01-18 09:51:40 +0800

    chore: 更新版本

--- src/js/common/ajax.js
+++ src/js/common/ajax.js
@@ -133,12 +133,6 @@ let ajax = {
         });
       })
     });
-  },
-  getContextPath: function() {
-    var pathName = document.location.pathname;
-    var index = pathName.substr(1).indexOf("/");
-    var result = pathName.substr(0, index + 1);
-    return result;
   }
 };
 module.exports = ajax;
