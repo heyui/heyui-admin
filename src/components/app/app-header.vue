c208abe6840ba70aab94aeef88d385d9aaeb4907
vvpvvp <alias8516@sina.com.cn>
2018-06-26 15:35:20 +0800

    docs: 样式优化

--- src/components/app/app-header.vue
+++ src/components/app/app-header.vue
@@ -1,8 +1,9 @@
 <template>
   <div class="app-header">
-    <div class="app-header-title"><a href="http://www.heyui.top">HEYUI</a></div>
+    <div class="app-header-title"><a href="http://www.heyui.top">HEY UI</a></div>
     <div class="float-right app-header-info">
       <span><a v-font="20" href="https://github.com/heyui/heyui-admin" target="_blank"><i class="h-icon-github"></i></a></span>
+      <i class="h-split"></i>
       <DropdownMenu class-name="app-header-dropdown" placement="bottom-end"
                     :datas="infoMenu"
                     @onclick="trigger"><span>{{User.name}}</span></DropdownMenu>
@@ -17,6 +18,7 @@ export default {
   data() {
     return {
       infoMenu: [
+        { key: 'info', title: '个人信息', icon: 'h-icon-user' },
         { key: 'logout', title: '退出登录', icon: 'h-icon-outbox' },
       ]
     }
