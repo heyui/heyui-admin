4fb0447a8105c6345050c9d3bf4b9e4be8aeec4d
vvpvvp <alias8516@sina.com.cn>
2018-11-20 00:02:18 +0800

    fix: systabs

--- src/components/common/SysTabs/SysTabs.vue
+++ src/components/common/SysTabs/SysTabs.vue
@@ -1,22 +1,15 @@
 <style lang="less">
-  .no-select{
-  -webkit-touch-callout: none;
-  -webkit-user-select: none;
-  -khtml-user-select: none;
-  -moz-user-select: none;
-  -ms-user-select: none;
-  user-select: none;
-}
-.size{
-  width: 100%;
-  height: 100%;
-}
 .sys-tabs-vue{
-  position: relative;
+  // position: relative;
   border-top: 1px solid #F0F0F0;
   border-bottom: 1px solid #F0F0F0;
-  .no-select;
-  .size;
+  right: 0;
+  position: fixed;
+  height: 45px;
+  top: 60px;
+  left: 240px;
+  background: #eee;
+  user-select: none;
   .close-con{
     position: absolute;
     right: 0;
@@ -27,34 +20,16 @@
     text-align: center;
     z-index: 10;
   }
-  .btn-con{
+  .tabs-container{
     position: absolute;
-    top: 0px;
-    height: 100%;
-    background: #fff;
-    padding-top: 3px;
-    z-index: 10;
-    button{
-      padding: 6px 4px;
-      line-height: 14px;
-      text-align: center;
-    }
-    &.left-btn{
-      left: 0px;
-    }
-    &.right-btn{
-      right: 32px;
-      border-right: 1px solid #F0F0F0;
-    }
-  }
-  .scroll-outer{
-    position: absolute;
-    left: 28px;
-    right: 61px;
+    // left: 28px;
+    // right: 61px;
+    left: 0;
+    right: 0;
     top: 0;
     bottom: 0;
     box-shadow: 0px 0 3px 2px rgba(100,100,100,.1) inset;
-    .scroll-body{
+    .tabs-body{
       height: ~"calc(100% - 1px)";
       display: inline-block;
       padding: 1px 4px 0;
@@ -62,9 +37,6 @@
       overflow: visible;
       white-space: nowrap;
       transition: left .3s ease;
-      .ivu-tag-dot-inner{
-        transition: background .2s ease;
-      }
     }
   }
   .contextmenu {
@@ -90,47 +62,12 @@
 
 <template>
   <div class="sys-tabs-vue">
-    <div class="close-con">
-      <Dropdown transfer @on-click="handleTagsOption" style="margin-top:7px;">
-        <Button size="small" type="text">
-          <Icon :size="18" type="ios-close-circle-outline" />
-        </Button>
-        <DropdownMenu slot="list">
-          <DropdownItem name="close-all">关闭所有</DropdownItem>
-          <DropdownItem name="close-others">关闭其他</DropdownItem>
-        </DropdownMenu>
-      </Dropdown>
-    </div>
-    <ul v-show="visible" :style="{left: contextMenuLeft + 'px', top: contextMenuTop + 'px'}" class="contextmenu">
-      <li v-for="(item, key) of menuList" @click="handleTagsOption(key)" :key="key">{{item}}</li>
-    </ul>
-    <div class="btn-con left-btn">
-      <Button type="text" @click="handleScroll(240)">
-        <Icon :size="18" type="ios-arrow-back" />
-      </Button>
-    </div>
-    <div class="btn-con right-btn">
-      <Button type="text" @click="handleScroll(-240)">
-        <Icon :size="18" type="ios-arrow-forward" />
-      </Button>
-    </div>
-    <div class="scroll-outer" ref="scrollOuter" @DOMMouseScroll="handlescroll" @mousewheel="handlescroll">
-      <div ref="scrollBody" class="scroll-body" :style="{left: tagBodyLeft + 'px'}">
-        <transition-group name="taglist-moving-animation">
-          <Tag
-            type="dot"
-            v-for="(item, index) in list"
-            ref="tagsPageOpened"
-            :key="`tag-nav-${index}`"
-            :name="item.name"
-            :data-route-item="item"
-            @on-close="handleClose(item)"
-            @click.native="handleClick(item)"
-            :closable="item.name !== $config.homeName"
-            :color="isCurrentTag(item) ? 'primary' : 'default'"
-            @contextmenu.prevent.native="contextMenu(item, $event)"
-          >{{ showTitleInside(item) }}</Tag>
-        </transition-group>
+    <div class="tabs-container" ref="scrollOuter">
+      <div ref="scrollBody" class="tabs-body" :style="{left: tagBodyLeft + 'px'}">
+        <span v-for="(item, index) of list" :key="`sys-tab-${index}`" @click="handleClick(item)" :class="{'chosen': isCurrentTab(item)}">
+          <span>{{showTitleInside(item)}}</span>
+          <span class="h-icon-close" @click.stop="handleClose(item)"></span>
+        </span>
       </div>
     </div>
   </div>
@@ -151,64 +88,16 @@ export default {
   },
   data () {
     return {
-      tagBodyLeft: 0,
-      rightOffset: 40,
-      outerPadding: 4,
-      contextMenuLeft: 0,
-      contextMenuTop: 0,
-      visible: false,
-      menuList: {
-        others: '关闭其他',
-        all: '关闭所有'
-      }
+      tagList: []
     }
   },
   computed: {
     currentRouteObj () {
-      const { name, params, query } = this.value
+      const { name, params, query } = this.$route;
       return { name, params, query }
     }
   },
   methods: {
-    handlescroll (e) {
-      var type = e.type
-      let delta = 0
-      if (type === 'DOMMouseScroll' || type === 'mousewheel') {
-        delta = (e.wheelDelta) ? e.wheelDelta : -(e.detail || 0) * 40
-      }
-      this.handleScroll(delta)
-    },
-    handleScroll (offset) {
-      const outerWidth = this.$refs.scrollOuter.offsetWidth
-      const bodyWidth = this.$refs.scrollBody.offsetWidth
-      if (offset > 0) {
-        this.tagBodyLeft = Math.min(0, this.tagBodyLeft + offset)
-      } else {
-        if (outerWidth < bodyWidth) {
-          if (this.tagBodyLeft < -(bodyWidth - outerWidth)) {
-            this.tagBodyLeft = this.tagBodyLeft
-          } else {
-            this.tagBodyLeft = Math.max(this.tagBodyLeft + offset, outerWidth - bodyWidth)
-          }
-        } else {
-          this.tagBodyLeft = 0
-        }
-      }
-    },
-    handleTagsOption (type) {
-      if (type.includes('all')) {
-        // 关闭所有，除了home
-        let res = this.list.filter(item => item.name === this.$config.homeName)
-        this.$emit('on-close', res, 'all')
-      } else if (type.includes('others')) {
-        // 关闭除当前页和home页的其他页
-        let res = this.list.filter(item => routeEqual(this.currentRouteObj, item) || item.name === this.$config.homeName)
-        this.$emit('on-close', res, 'others', this.currentRouteObj)
-        setTimeout(() => {
-          this.getTagElementByName(this.currentRouteObj.name)
-        }, 100)
-      }
-    },
     beforeClose() {
       return this.$Confirm('确定要关闭这一页吗');
     },
@@ -233,65 +122,15 @@ export default {
     showTitleInside (item) {
       return showTitle(item, this)
     },
-    isCurrentTag (item) {
+    isCurrentTab (item) {
       return routeEqual(this.currentRouteObj, item)
-    },
-    moveToView (tag) {
-      const outerWidth = this.$refs.scrollOuter.offsetWidth
-      const bodyWidth = this.$refs.scrollBody.offsetWidth
-      if (bodyWidth < outerWidth) {
-        this.tagBodyLeft = 0
-      } else if (tag.offsetLeft < -this.tagBodyLeft) {
-        // 标签在可视区域左侧
-        this.tagBodyLeft = -tag.offsetLeft + this.outerPadding
-      } else if (tag.offsetLeft > -this.tagBodyLeft && tag.offsetLeft + tag.offsetWidth < -this.tagBodyLeft + outerWidth) {
-        // 标签在可视区域
-        this.tagBodyLeft = Math.min(0, outerWidth - tag.offsetWidth - tag.offsetLeft - this.outerPadding)
-      } else {
-        // 标签在可视区域右侧
-        this.tagBodyLeft = -(tag.offsetLeft - (outerWidth - this.outerPadding - tag.offsetWidth))
-      }
-    },
-    getTagElementByName (route) {
-      this.$nextTick(() => {
-        this.refsTag = this.$refs.tagsPageOpened
-        this.refsTag.forEach((item, index) => {
-          if (routeEqual(route, item.$attrs['data-route-item'])) {
-            let tag = this.refsTag[index].$el
-            this.moveToView(tag)
-          }
-        })
-      })
-    },
-    contextMenu (item, e) {
-      if (item.name === this.$config.homeName) {
-        return
-      }
-      this.visible = true
-      const offsetLeft = this.$el.getBoundingClientRect().left
-      this.contextMenuLeft = e.clientX - offsetLeft + 10
-      this.contextMenuTop = e.clientY - 64
-    },
-    closeMenu () {
-      this.visible = false
     }
   },
   watch: {
     '$route' (to) {
-      this.getTagElementByName(to)
-    },
-    visible (value) {
-      if (value) {
-        document.body.addEventListener('click', this.closeMenu)
-      } else {
-        document.body.removeEventListener('click', this.closeMenu)
-      }
     }
   },
   mounted () {
-    setTimeout(() => {
-      this.getTagElementByName(this.$route)
-    }, 200)
   }
 }
 </script>
