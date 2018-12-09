34932fc39b43b344fcf6e4258da048c726f829db
vvpvvp <alias8516@sina.com.cn>
2018-12-10 00:36:56 +0800

    fix: mock data

--- src/mock/index.js
+++ src/mock/index.js
@@ -8,6 +8,6 @@ Mock.mock('/api/account/info', "get", {"status":200,"body":{"name":"moni"}});
 
 Mock.mock('/api/dict', "get", {"status":200,"body":[{"name":"simple","data":{"1":"苹果","2":"梨子","3":"香蕉","4":"橙子","5":"樱桃"}}]});
 
-Mock.mock('/api/login', "post", {"status":200});
+Mock.mock('/api/login', "post", {"status":200, "body": {"value": "test"}});
 
 Mock.mock('/api/logout', "post", {"status":200});
