2f894960b5ce08d67d506b67792831d22da80177
vvpvvp <alias8516@sina.com.cn>
2019-02-01 11:52:32 +0800

    feat: init search page

--- src/components/demo-components/table/search.vue
+++ src/components/demo-components/table/search.vue
@@ -1,8 +1,11 @@
 <style lang='less'>
 </style>
 <template>
-  <div class="">
-    
+  <div class="search-list-vue frame-page h-panel">
+    <div class="h-panel-bar"><span class="h-panel-title">查询列表 </span></div>
+    <div class="h-panel-body">
+      
+    </div>
   </div>
 </template>
 <script>
