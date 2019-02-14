38e7bc1fcbef4b5e0e2947ac32b4f1efbde81517
vvpvvp <alias8516@sina.com.cn>
2019-02-14 11:39:31 +0800

    fix: use mock always

--- src/app.js
+++ src/app.js
@@ -8,11 +8,14 @@ import 'js/vue/components';
 
 require('./css/app.less');
 
-process.env.NODE_ENV == 'development' && require('./mock')
+// 开发环境判断
+// process.env.NODE_ENV == 'development'
 
-//HeyUI已经设定为全局变量，无需引用
-//设定全局变量请参考根目录下的hey.conf.js文件
+// 使用mock文件， 自己开发的时候请删除
+require('./mock')
 
+// HeyUI已经设定为全局变量，无需引用
+// 设定全局变量请参考根目录下的hey.conf.js文件
 
 heyuiConfig();
 Vue.use(HeyUI);
