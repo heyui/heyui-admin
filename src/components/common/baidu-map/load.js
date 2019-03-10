ce3dfa4984c8b3c94f75e8b77e8bf30472555c49
vvpvvp <alias8516@sina.com.cn>
2019-03-10 14:36:08 +0800

    fix: 将百度api调用改成https

--- src/components/common/baidu-map/load.js
+++ src/components/common/baidu-map/load.js
@@ -2,7 +2,7 @@ export default function() {
   return new Promise(function (resolve, reject) {
     var script = document.createElement("script");
     script.type = "text/javascript";
-    script.src = `//api.map.baidu.com/getscript?v=2.0&ak=20qOZbvLhZnFinXiG1NfGPLC&t=${new Date().getTime()}`;
+    script.src = `https://api.map.baidu.com/getscript?v=2.0&ak=20qOZbvLhZnFinXiG1NfGPLC&t=${new Date().getTime()}`;
     script.onerror = reject;
     document.head.appendChild(script);
     script.onload = () => {
