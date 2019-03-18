2322cacfc80e4039fce01a879b5f610a1a66fba7
vvpvvp <alias8516@sina.com.cn>
2019-03-18 16:52:40 +0800

    fix: update vue.config.js
    Closed #12

--- vue-cli/vue.config.js
+++ vue-cli/vue.config.js
@@ -39,11 +39,12 @@ module.exports = {
     ]
   },
   devServer: {
-    proxy: {
-      // 此处应该配置为开发服务器的后台地址
-      // '/api': {
-      //   target: 'http://xxx.xx.xx'
-      // }
-    }
+    // proxy: {
+    // 此处应该配置为开发服务器的后台地址
+    // 配置文档： https://cli.vuejs.org/zh/config/#devserver-proxy
+    // '/api': {
+    //   target: 'http://xxx.xx.xx'
+    // }
+    // }
   }
 };
