06bf3adfbdc86f0729e022f7678ae782663a9723
vvpvvp <alias8516@sina.com.cn>
2019-03-28 11:15:39 +0800

    feat: update login page

--- src/components/login/index.vue
+++ src/components/login/index.vue
@@ -1,12 +1,14 @@
 <style lang="less">
 @gradient-color: #3788ee;
 @bg-color: #f7f8fa;
-@text-color: #1d1d1d;
+@title-color:#3a3a3a;
+@text-color: #7e7e7e;
+@placeholder-color: #7e7e7e;
 .login-vue {
   text-align: center;
   position: absolute;
   top: 0;
-  bottom: 0; 
+  bottom: 0;
   right: 0;
   left: 0;
   background: @bg-color;
@@ -20,42 +22,53 @@
       letter-spacing: 2px;
       background: #FFF;
       padding: 70px 30px 20px;
-      box-shadow: 0 1px 3px 0 #0000000f;
+      box-shadow: 0 1px 3px 0 rgba(0, 0, 0, 0.06);
       border-radius: 3px;
       box-sizing: border-box;
       >div {
         margin: 30px 0;
-        label {
-          margin-right: 10px;
-          line-height: 35px;
-          font-size: 18px;
-          color: @gray-color;
-        }
-        input {
-          font-size: 16px;
-          padding: 8px 0 8px 8px;
-          height: 40px;
-          width: 100%;
-          border: none;
-          border-radius: 0;
-          border-bottom: 1px solid #d3d3d3;
-          box-shadow: inset 0 0 0 1000px #fff;
-          outline: none;
-          box-sizing: border-box;
-          transition: .3s;
-          font-weight: 200;
-          &:focus {
-            border-bottom-color: @gradient-color;
+        &.login-input {
+          position: relative;
+          .placeholder {
+            position: absolute;
+            color: @placeholder-color;
+            top: 6px;
+            font-size: 16px;
+            transition: top .2s;
+            left: 0;
+            pointer-events: none;
+          }
+          input {
+            font-size: 16px;
+            padding: 8px 0;
+            height: 40px;
+            width: 100%;
+            border: none;
+            border-radius: 0;
+            border-bottom: 1px solid #d3d3d3;
             box-shadow: inset 0 0 0 1000px #fff;
+            outline: none;
+            box-sizing: border-box;
+            transition: .3s;
+            font-weight: 200;
+            &:focus {
+              border-bottom-color: @gradient-color;
+              box-shadow: inset 0 0 0 1000px #fff;
+            }
+          }
+          input:focus + .placeholder,input:-webkit-autofill + .placeholder, .placeholder.fixed{
+            font-size: 13px;
+            top: -16px;
           }
         }
         &.login-title {
           font-size: 30px;
-          color: @text-color;
+          color: @title-color;
           line-height: 1;
           margin: -16px 0px 40px;
           font-weight: 200;
         }
+
       }
       > .buttonDiv {
         margin-top: 45px;
@@ -63,6 +76,7 @@
           padding: 12px 0;
           font-size: 18px;
           opacity: .8;
+          border-radius: 3px;
           background: @gradient-color;
           border-color: @gradient-color;
           &:hover {
@@ -90,17 +104,19 @@
     <div class="login-container">
       <div class="login-content">
         <div class="login-title">管理系统</div>
-        <div class="login-name">
-          <input type="text" v-model="login.username" placeholder="用户名"/>
+        <div class="login-name login-input">
+          <input type="text" v-model="login.username" autocomplete="off"/>
+          <span class="placeholder" :class="{fixed: login.username != '' && login.username != null}">用户名</span>
         </div>
-        <div class="login-password">
-          <input type="password" v-model="login.password" placeholder="密码" @keyup.enter="submit"/>
+        <div class="login-password login-input">
+          <input type="password" v-model="login.password" @keyup.enter="submit" autocomplete="off"/>
+          <span class="placeholder" :class="{fixed: login.password != '' && login.password != null}">密码</span>
         </div>
         <div class="buttonDiv">
           <Button :loading="loading" block color="primary" size="l" @click="submit">登录</Button>
         </div>
       </div>
-      <p class="copyright"> Copyright © 2019 HeyUI. - <a href="https://www.heyui.top/">heyui.top</a></p>
+      <p class="copyright"> Copyright © 2019 vvpvvp - <a href="https://www.heyui.top/">heyui.top</a></p>
     </div>
   </div>
 </template>
@@ -114,6 +130,8 @@ export default {
       login: Login.parse({}),
       loading: false
     };
+  },
+  mounted() {
   },
   methods: {
     submit() {
