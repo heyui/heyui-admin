40daae88022d12d3d8795c06b1874c9afab68aaf
vvpvvp <alias8516@sina.com.cn>
2019-06-26 11:16:31 +0800

    feat: menu 切换使用 logo 做显隐

--- src/components/app/app-logo.vue
+++ src/components/app/app-logo.vue
@@ -1,7 +1,6 @@
 <style lang='less'>
 .app-logo{
   font-family: Futura,Helvetica Neue For Number,-apple-system,BlinkMacSystemFont,Segoe UI,Roboto,PingFang SC,Hiragino Sans GB,Microsoft YaHei,Helvetica Neue,Helvetica,Arial,sans-serif;
-  font-size: 20px;
   padding: 0px 24px;
   margin-bottom: 30px;
   line-height: @layout-header-height;
@@ -9,10 +8,32 @@
   overflow: hidden;
   white-space: nowrap;
   transition: padding .3s;
+  .app-logo-text {
+    font-size: 20px;
+    vertical-align: middle;
+  }
+  .app-logo-icon {
+    background: url(../../images/logo.png);
+    background-size: contain;
+    height: 26px;
+    width: 26px;
+    display: inline-block;
+    margin-right: 10px;
+    transition: .3s;
+    vertical-align: middle;
+  }
+}
+.h-layout-sider-collapsed {
+  .app-logo-text {
+    display: none;
+  }
+  .app-logo-icon {
+    margin-left: 15px;
+  }
 }
 </style>
 <template>
-  <div class="app-logo"><router-link to="/">HEYUI ADMIN</router-link></div>
+  <div class="app-logo"><router-link to="/"><span class="app-logo-icon"></span><span class="app-logo-text">HEYUI ADMIN</span></router-link></div>
 </template>
 <script>
 export default {
