3772ad3d5343e53129b627fe099e283742b8b36c
vvpvvp <alias8516@sina.com.cn>
2019-07-04 15:41:50 +0800

    fix: core-js 降级

--- src/app.js
+++ src/app.js
@@ -1,5 +1,6 @@
-import 'core-js/stable';
-import 'regenerator-runtime/runtime';
+// import 'core-js/stable';
+// import 'regenerator-runtime/runtime';
+import '@babel/polyfill';
 import Vue from 'vue';
 import App from 'components/App';
 
